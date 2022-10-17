137b7d7cd6b4e2cb2a04d887236ecf3c6de3136b
Mücahit <mucahittgedik0@icloud.com>
2022-10-17 19:48:35 +0300

    v1

---
+++ script.js
@@ -0,0 +1,18 @@
+const jokeEl = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+generateJoke();
+jokeBtn.addEventListener("click" , generateJoke)
+async function generateJoke(){
+    const config = {
+        headers : {
+            Accept : "application/json"
+        }
+    }
+    const response = await fetch("https://icanhazdadjoke.com", config)
+
+    const data = await response.json();
+
+    jokeEl.innerHTML = data.joke
+   
+}
